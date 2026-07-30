d54fa4b4e5870352a649f778efa9ab3ef01ae941
Nico Lehmann <nico.lehmannm@gmail.com>
2026-07-30 16:31:00 -0400

    update dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 use autograder::cli::Cli;
 use clap::Parser;
 
-fn main() -> anyhow::Result<()> {
+fn main() {
     tracing_subscriber::fmt()
         .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
         .init();
 
     let cli = Cli::parse();
-    autograder::dispatch(cli.command)?;
-    Ok(())
+    if let Err(err) = autograder::dispatch(cli.command) {
+        eprintln!("Error: {err}");
+        std::process::exit(1);
+    }
 }
